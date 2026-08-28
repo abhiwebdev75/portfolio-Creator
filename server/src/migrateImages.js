63dd9ad0730cbb87f94a4ad70dfeb95f969a65e9
Abhinash <abhit7575@gmail.com>
2026-08-28 19:13:24 +0530

    cloudinary file

---
+++ server/src/migrateImages.js
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+import { v2 as cloudinary } from 'cloudinary';
+import mongoose from 'mongoose';
+import connectDB from './config/db.js';
+import Project from './models/Project.js';
+import Certificate from './models/Certificate.js';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const sourceBaseUrl = (process.env.PUBLIC_SERVER_URL || '').replace(/\/$/, '');
+
+if (!sourceBaseUrl) {
+  throw new Error('PUBLIC_SERVER_URL must be set to the current Render API URL');
+}
+
+async function migrateModel(Model, label) {
+  const items = await Model.find({ imageUrl: /^\/uploads\// });
+
+  for (const item of items) {
+    const sourceUrl = `${sourceBaseUrl}${item.imageUrl}`;
+    const result = await cloudinary.uploader.upload(sourceUrl, {
+      folder: 'portfolio',
+      resource_type: 'image',
+    });
+
+    await Model.updateOne({ _id: item._id }, { $set: { imageUrl: result.secure_url } });
+    console.log(`${label}: migrated ${item._id}`);
+  }
+
+  console.log(`${label}: ${items.length} image(s) migrated`);
+}
+
+try {
+  await connectDB();
+  await migrateModel(Project, 'Projects');
+  await migrateModel(Certificate, 'Certificates');
+} finally {
+  await mongoose.disconnect();
+}
